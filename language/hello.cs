c2b73ebf5f5b5df651b4a75540d65a8d5387c4a3
Art Navsegda <artnavsegda@gmail.com>
2019-06-25 06:45:36 +0300

    Update hello.cs

--- language/hello.cs
+++ language/hello.cs
@@ -1,12 +1,9 @@
 ﻿using System;
 
-namespace corehello
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+        Console.WriteLine("Hello World!");
     }
 }
