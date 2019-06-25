3223a8b35151c77a767c3cfec6adff651d8aee70
Art Navsegda <artnavsegda@gmail.com>
2019-06-25 06:41:42 +0300

    windows forms

--- wfhello.cs
+++ wfhello.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Windows.Forms;
 
 class Program
 {
     static void Main(string[] args)
     {
+	MessageBox.Show("Hello World!");
         Console.WriteLine("Hello World!");
     }
 }
