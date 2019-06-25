1cf67f7b222e80cf1707b0d0467ab622286169a0
Art Navsegda <artnavsegda@gmail.com>
2019-06-25 08:24:04 +0300

    Update operator.cs

--- language/operator.cs
+++ language/operator.cs
@@ -5,12 +5,25 @@ class Program
     static void Main(string[] args)
     {
 	int i;
-	i = 42;
+	i = 42 + 20;
+        Console.WriteLine(i);
+	i = 42 - 20;
+        Console.WriteLine(i);
+	i = 42 * 2;
+        Console.WriteLine(i);
+	i = 42 / 2;
+        Console.WriteLine(i);
+	i = 5 % 3;
         Console.WriteLine(i);
 	float x;
-	x = 26.32F;
+	x = 26.32F + 10.0F;
         Console.WriteLine(x);
-        Console.WriteLine(x.ToString());
+	x = 26.32F - 10.0F;
+        Console.WriteLine(x);
+	x = 13F * 1.5F;
+        Console.WriteLine(x);
+	x = 13F / 2F;
+        Console.WriteLine(x);
 	string f;
        	f = "hello" + "world";
         Console.WriteLine(f);
