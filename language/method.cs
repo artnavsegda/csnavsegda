89da7554e829aeed56e525a6ede51ae7af1e68c0
Art Navsegda <artnavsegda@gmail.com>
2019-06-25 08:43:02 +0300

    Create method.cs

---
+++ language/method.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void printhello()
+    {
+        Console.WriteLine("Hello World!");
+    }
+    static void Main(string[] args)
+    {
+	printhello();
+    }
+}
