585732d5d2595eec6929b24278a812a5049f6780
Art Navsegda <artnavsegda@gmail.com>
2019-07-01 04:53:33 +0300

    Create class.cs

---
+++ language/class.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+	myclass myobj = new myclass();
+	myobj.printhello();
+    }
+}
+
+public class myclass
+{
+    public void printhello()
+    {
+        Console.WriteLine("Hello World!");
+    }
+}
