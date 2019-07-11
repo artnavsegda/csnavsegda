0c4b318e61cf1efba7c3dce77cfa65307bc9c947
Art Navsegda <artnavsegda@gmail.com>
2019-07-11 04:03:27 +0300

    array foreach

---
+++ language/array.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+	int[] myarray = new int[10];
+	myarray[0] = 12;
+        Console.WriteLine(myarray[0]);
+	myarray[1] = 34;
+        Console.WriteLine(myarray[1]);
+	int[] noarray = { 1, 2, 3 };
+        Console.WriteLine(noarray[1]);
+    }
+}
---
+++ language/foreach.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+	int[] noarray = { 1, 2, 3 };
+	foreach (int i in noarray)
+	{
+            Console.WriteLine(i);
+	}
+    }
+}
