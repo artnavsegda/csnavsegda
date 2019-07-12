412db83ecf46b084bd9dd5a62e5dc13759787682
Art Navsegda <artnavsegda@gmail.com>
2019-07-12 14:50:23 +0300

    Create write.cs

---
+++ library/write.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+	    StreamWriter sw = File.CreateText("test.txt");
+            sw.WriteLine("Hello World!");
+    }
+}
