f64f447db94160e3500e4d28ba79bcf7e9059283
Art Navsegda <artnavsegda@gmail.com>
2019-07-13 00:53:13 +0300

    Create resolve.cs

---
+++ library/socket/resolve.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Net.Sockets;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+	Socket mysock = new Socket(SocketType.Stream, ProtocolType.Tcp);
+        Console.WriteLine("Hello World!");
+    }
+}
