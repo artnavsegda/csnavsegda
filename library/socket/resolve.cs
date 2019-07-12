af537e32198946618558aa42d0ca6fbaabb0ce48
Art Navsegda <artnavsegda@gmail.com>
2019-07-13 01:43:04 +0300

    Update resolve.cs

--- library/socket/resolve.cs
+++ library/socket/resolve.cs
@@ -1,11 +1,12 @@
 ﻿using System;
+using System.Net;
 using System.Net.Sockets;
 
 class Program
 {
     static void Main(string[] args)
     {
-	Socket mysock = new Socket(SocketType.Stream, ProtocolType.Tcp);
-        Console.WriteLine("Hello World!");
+	IPHostEntry host = Dns.GetHostEntry("artnavsegda.herokuapp.com");
+        Console.WriteLine(host.AddressList[0]);
     }
 }
