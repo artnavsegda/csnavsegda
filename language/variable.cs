9d5bddc6104ac6467bef3936f0c1c24b74ffcf87
Art Navsegda <artnavsegda@gmail.com>
2019-07-13 19:42:32 +0300

    boolean

--- language/variable.cs
+++ language/variable.cs
@@ -2,23 +2,26 @@
 
 class Program
 {
-    static void Main(string[] args)
-    {
-	int i;
-	i = 42;
-        Console.WriteLine(i);
-	float x;
-	x = 26.32F;
-        Console.WriteLine(x);
-        Console.WriteLine(x.ToString());
-	string f;
-       	f = "32";
-        Console.WriteLine(f);
-        Console.WriteLine(System.Int32.Parse(f));
-	char y;
-	y = 'x';
-        Console.WriteLine(y);
-	var o = 24;
-        Console.WriteLine(o);
-    }
+	static void Main(string[] args)
+	{
+		bool a;
+		a = true;
+		Console.WriteLine(a);
+		int i;
+		i = 42;
+		Console.WriteLine(i);
+		float x;
+		x = 26.32F;
+		Console.WriteLine(x);
+		Console.WriteLine(x.ToString());
+		string f;
+		f = "32";
+		Console.WriteLine(f);
+		Console.WriteLine(System.Int32.Parse(f));
+		char y;
+		y = 'x';
+		Console.WriteLine(y);
+		var o = 24;
+		Console.WriteLine(o);
+	}
 }
