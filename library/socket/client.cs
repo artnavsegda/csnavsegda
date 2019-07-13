78b23bf8d90fae4c79fa799af7c94115b8d087ac
Art Navsegda <artnavsegda@gmail.com>
2019-07-13 04:14:22 +0300

    this

--- library/socket/client.cs
+++ library/socket/client.cs
@@ -1,11 +1,15 @@
 ﻿using System;
+using System.Net;
 using System.Net.Sockets;
 
 class Program
 {
     static void Main(string[] args)
     {
-	Socket mysock = new Socket(SocketType.Stream, ProtocolType.Tcp);
+	IPAddress myaddr = IPAddress.Parse("127.0.0.1");
+	IPEndPoint endpoint = new IPEndPoint(myaddr, 8888);
+        Socket mysock = new Socket(SocketType.Stream, ProtocolType.Tcp);
+	mysock.Connect(endpoint);
         Console.WriteLine("Hello World!");
     }
 }
--- library/socket/webclient.cs
+++ library/socket/webclient.cs
@@ -1,11 +1,12 @@
 ﻿using System;
-using System.Net;
+using System.Net.Sockets;
 
 class Program
 {
     static void Main(string[] args)
     {
         IPHostEntry host = Dns.GetHostEntry("artnavsegda.herokuapp.com");
-        Console.WriteLine(host.AddressList[0]);
+        Socket mysock = new Socket(SocketType.Stream, ProtocolType.Tcp);
+        Console.WriteLine("Hello World!");
     }
 }
