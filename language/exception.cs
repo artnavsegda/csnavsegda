946e55b9ef16ce9de04c54ad15e5145fc7dac594
Art Navsegda <artnavsegda@gmail.com>
2019-07-14 21:40:14 +0300

    Create exception.cs

---
+++ language/exception.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+	static void myfunc2(int number)
+	{
+		if (number == 34)
+		{
+			throw new ArithmeticException("Number is thirty four");
+		}
+		Console.WriteLine(number);
+	}
+	static void myfunc(int number)
+	{
+		try
+		{
+			myfunc2(number);
+		}
+		catch (Exception e)
+		{
+			Console.WriteLine("error");
+		}
+	}
+	static void Main(string[] args)
+	{
+		myfunc(22);
+		myfunc(34);
+	}
+}
