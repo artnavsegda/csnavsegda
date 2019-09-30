6e0d00233d5bff905259edb0e1df4641d995a895
Art Navsegda <artnavsegda@gmail.com>
2019-10-01 02:21:54 +0300

    Create enum.cs

---
+++ language/enum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Enumerations
+{
+	enum numbers {one, two, three, four, five};
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			numbers mynumber;
+			mynumber = numbers.one;
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
