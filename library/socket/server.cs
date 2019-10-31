b207c51c69efbb535849060fc67e1976ed002f5f
Art Navsegda <artnavsegda@gmail.com>
2019-10-31 16:43:49 +0300

    server

--- library/socket/server.cs
+++ library/socket/server.cs
@@ -5,12 +5,16 @@ using System;
 class Server {
     public static void Main() {
         TcpListener server = new TcpListener(IPAddress.Parse("127.0.0.1"), 80);
-
         server.Start();
         Console.WriteLine("Server has started on 127.0.0.1:80.{0}Waiting for a connection...", Environment.NewLine);
-
         TcpClient client = server.AcceptTcpClient();
-
         Console.WriteLine("A client connected.");
+        NetworkStream stream = client.GetStream();
+        //enter to an infinite cycle to be able to handle every change in stream
+        while (true) {
+          while (!stream.DataAvailable);
+          Byte[] bytes = new Byte[client.Available];
+          stream.Read(bytes, 0, bytes.Length);
+        }
     }
 }
