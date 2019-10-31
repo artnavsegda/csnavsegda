3d2a55d804c950a44fbccdff2aaebea8339832c1
Art Navsegda <artnavsegda@gmail.com>
2019-10-31 16:51:56 +0300

    decode

--- library/socket/websocket-server.cs
+++ library/socket/websocket-server.cs
@@ -21,8 +21,29 @@ class Server {
         stream.Read(bytes, 0, bytes.Length);
         //translate bytes of request to string
         String data = Encoding.UTF8.GetString(bytes);
-        if (Regex.IsMatch(data, "^GET")) {
-        } else {
+        if (new System.Text.RegularExpressions.Regex("^GET").IsMatch(data))
+        {
+          const string eol = "\r\n"; // HTTP/1.1 defines the sequence CR LF as the end-of-line marker
+          Byte[] response = Encoding.UTF8.GetBytes("HTTP/1.1 101 Switching Protocols" + eol
+              + "Connection: Upgrade" + eol
+              + "Upgrade: websocket" + eol
+              + "Sec-WebSocket-Accept: " + Convert.ToBase64String(
+                System.Security.Cryptography.SHA1.Create().ComputeHash(
+                  Encoding.UTF8.GetBytes(
+                    new System.Text.RegularExpressions.Regex("Sec-WebSocket-Key: (.*)").Match(data).Groups[1].Value.Trim() + "258EAFA5-E914-47DA-95CA-C5AB0DC85B11"
+                    )
+                  )
+                ) + eol
+              + eol);
+          stream.Write(response, 0, response.Length);
+        }
+
+        Byte[] decoded = new Byte[3];
+        Byte[] encoded = new Byte[3] {112, 16, 109};
+        Byte[] mask = new Byte[4] {61, 84, 35, 6};
+
+        for (int i = 0; i < encoded.Length; i++) {
+          decoded[i] = (Byte)(encoded[i] ^ mask[i % 4]);
         }
     }
 }
