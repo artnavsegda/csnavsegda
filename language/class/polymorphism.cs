bfcb33f186f61740ba798475c4211150ff4bb839
Art Navsegda <artnavsegda@gmail.com>
2019-12-18 17:43:19 +0300

    Create polymorphism.cs

---
+++ language/class/polymorphism.cs
@@ -0,0 +1,34 @@
+using System;
+
+public class myclass
+{
+  public int i;
+  public void printsome()
+  {
+    Console.WriteLine(i);
+  }
+}
+
+public class otherclass : myclass
+{
+  public int x;
+  public new void printsome()
+  {
+    Console.WriteLine(i*x);
+  }
+}
+
+class Program
+{
+  static void Main(string[] args)
+  {
+    myclass myobj = new myclass();
+    myobj.i = 10;
+    myobj.printsome();
+
+    otherclass myobj3 = new otherclass();
+    myobj3.i = 11;
+    myobj3.x = 2;
+    myobj3.printsome();
+  }
+}
