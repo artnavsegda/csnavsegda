d787f784838ab556304ab4039f3e1d6ccf937022
Art Navsegda <artnavsegda@gmail.com>
2019-12-18 16:42:55 +0300

    Update class.cs

--- language/class/class.cs
+++ language/class/class.cs
@@ -2,17 +2,17 @@
 
 class Program
 {
-    static void Main(string[] args)
-    {
-	myclass myobj = new myclass();
-	myobj.printhello();
-    }
+  static void Main(string[] args)
+  {
+    myclass myobj = new myclass();
+    myobj.printhello();
+  }
 }
 
 public class myclass
 {
-    public void printhello()
-    {
-        Console.WriteLine("Hello World!");
-    }
+  public void printhello()
+  {
+    Console.WriteLine("Hello World!");
+  }
 }
