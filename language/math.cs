ee38b07224699463d480bf9cd31f150b2abcda0f
Art Navsegda <artnavsegda@gmail.com>
2020-06-26 10:10:33 +0300

    Create math.cs

---
+++ language/math.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		int i;
+		i = (int)(((float)1000 / (float)10000) * (float)1000);
+		Console.WriteLine("Result: " + i);
+	}
+}
