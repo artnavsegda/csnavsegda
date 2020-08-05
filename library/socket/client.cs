282f09fd027ca1260a52f85cf8f56f2202fc6dc6
Art Navsegda <artnavsegda@gmail.com>
2020-08-05 17:09:19 +0300

    Update client.cs

--- library/socket/client.cs
+++ library/socket/client.cs
@@ -7,8 +7,8 @@ class Program
 {
 	static void Main(string[] args)
 	{
-		IPAddress myaddr = IPAddress.Parse("127.0.0.1");
-		IPEndPoint endpoint = new IPEndPoint(myaddr, 8888);
+		IPAddress myaddr = IPAddress.Parse("192.168.88.41");
+		IPEndPoint endpoint = new IPEndPoint(myaddr, 9999);
 		Socket mysock = new Socket(endpoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
 		mysock.Connect(endpoint);
 		if (mysock.Connected)
