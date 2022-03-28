7adf371b8764545e0c855333c1601fad394d827e
Art Navsegda <artnavsegda@gmail.com>
2022-03-28 12:01:40 +0300

    ok wtf

--- avia/Program.cs
+++ avia/Program.cs
@@ -1,3 +1,8 @@
+using GenHTTP.Engine;
+
+using GenHTTP.Modules.IO;
+using GenHTTP.Modules.Practices;
+
 namespace avia
 {
     internal static class Program
@@ -10,6 +15,15 @@ namespace avia
         {
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
+
+            var content = Content.From(Resource.FromString("Hello World!"));
+
+            Host.Create()
+               .Console()
+               .Defaults()
+               .Handler(content)
+               .Run();
+
             ApplicationConfiguration.Initialize();
             Application.Run(new Form1());
         }
